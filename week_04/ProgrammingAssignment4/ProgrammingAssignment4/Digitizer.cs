1dac29e86b68b763afddd0d2853635f5f640e6b8
Md Kamruzzaman Tusher <8888749+tkamruzzaman@users.noreply.github.com>
2023-10-11 13:43:12 +0600

    ProgrammingAssignment4

---
+++ week_04/ProgrammingAssignment4/ProgrammingAssignment4/Digitizer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingAssignment4
+{
+    /// <summary>
+    /// Converts words to digits
+    /// </summary>
+    public class Digitizer
+    {
+        #region Fields
+
+        public Dictionary<string, int> digitizerDictionary;
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public Digitizer()
+        {
+            digitizerDictionary = new Dictionary<string, int>()
+            {
+                {"zero", 0 },
+                {"one", 1 },
+                {"two", 2 },
+                {"three", 3 },
+                {"four", 4 },
+                {"five", 5 },
+                {"six", 6 },
+                {"seven", 7 },
+                {"eight", 8 },
+                {"nine", 9 }
+            };
+
+        }
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Converts the given word to the corresponding digit.
+        /// If the word isn't a valid digit name, returns -1
+        /// </summary>
+        /// <param name="word">word to convert</param>
+        /// <returns>corresponding digit or -1</returns>
+        public int ConvertWordToDigit(string word)
+        {
+            word = word.ToLower();
+            if (digitizerDictionary.ContainsKey(word)) { return digitizerDictionary[word]; }
+            return -1;
+        }
+
+        #endregion
+    }
+}
---
+++ week_04/ProgrammingAssignment4/ProgrammingAssignment4/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ProgrammingAssignment4
+{
+    // Don't change ANY of the code in this file; if you
+    // do, you'll break the automated grader!
+
+    /// <summary>
+    /// Programming Assignment 4
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Tests the Digitizer class
+        /// </summary>
+        /// <param name="args">command-line args</param>
+        static void Main(string[] args)
+        {
+            Digitizer digitizer = new Digitizer();
+
+            // loop while there's more input
+            string input = Console.ReadLine();
+            while (input[0] != 'q')
+            {
+                // convert word to digit and print
+                Console.WriteLine(digitizer.ConvertWordToDigit(input));
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
